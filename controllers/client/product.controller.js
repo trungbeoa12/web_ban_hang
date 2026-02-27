ff306a6c8f7e91f372203d99a2e317cf5b1676d0
Do Thanh Trung <dttrung.neu@gmail.com>
2026-02-27 09:39:20 +0700

    lam trang danh sach san pham ben phia client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,20 @@
-module.exports.index = (req, res) => {
-    res.render("client/pages/products/index.pug", {
-        pageTitle: "Danh sach san pham",
+const Product = require("../../models/product.model.js")
+
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        status: "inactive",
+        deleted: false
     });
-}
+
+    const newProducts = products.map(item => {
+        item.priceNew = (item.price * (100 - item.discountPercentage)/100).toFixed(0);
+        return item;
+    });
+
+    console.log(newProducts);
+
+    res.render("client/pages/products/index", {
+        pageTitle: "Danh sach san pham",
+        products: newProducts
+    });
+};
