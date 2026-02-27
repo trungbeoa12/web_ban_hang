080cc8611af0e7330660d011f490d8140ab9c772
Do Thanh Trung <dttrung.neu@gmail.com>
2026-02-27 15:00:54 +0700

    cau truc layouts chung

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 
 const database = require("./config/database.js");
 
+const systemConfig = require("./config/system.js")
+
 const routeAdmin = require("./routers/admin/index.route.js");
 const route = require("./routers/client/index.route.js");
 
@@ -13,6 +15,10 @@ const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+// App Locals variable
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
 
 app.use(express.static("public"));
 
