ed9973f9933e83a88b4db7566c809b47e3e8b524
Do Thanh Trung <dttrung.neu@gmail.com>
2026-02-27 15:46:33 +0700

    dung khung admin

--- routers/admin/index.route.js
+++ routers/admin/index.route.js
@@ -1,7 +1,14 @@
 const systemConfig = require("../../config/system.js");
+
 const dashboardRouters = require("./dashboard.route.js");
+const productRouters = require("./product.route.js");
+
 
 module.exports = (app) => {
     const PATH_ADMIN = systemConfig.prefixAdmin;
+
     app.use(PATH_ADMIN + "/dashboard", dashboardRouters);
+
+    app.use(PATH_ADMIN + "/products", productRouters);
+
 }
